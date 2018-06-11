312be3bf1e7287b9c3c7918997e75442a17d3f85
Veaudour Mickael <mckmonster@gmail.com>
2018-06-11 21:10:54 +0200

    First commit

---
+++ Rocket.Chat.RestAPI/Data/Authoring.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rocket.Chat.RestAPI.Data
+{
+    internal class Authoring
+    {
+        public string UserId { get; set; }
+
+        public string AuthToken { get; set; }
+    }
+}
---
+++ Rocket.Chat.RestAPI/Data/RequestResult.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rocket.Chat.RestAPI.Data
+{
+    internal class RequestResult<T>
+    {
+        public string Status { get; set; }
+        public bool Success { get; set; }
+        public string Error { get; set; }
+        public string ErrorType { get; set; }
+
+        public T Data { get; set; }
+        public T Info { get; set; }
+        public T Channel { get; set; }
+    }
+}
